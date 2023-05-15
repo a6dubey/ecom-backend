880fc541c934f65b40601fb5a6b648e021124815
ABHISHEK DUBEY <abhishekdubey@MacBook.local>
2023-05-15 09:08:24 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose.connect(process.env.DB_URI)
+  .then(()=>{
+    console.log("Connected to Database");
+  })
+  .catch(()=>{
+    console.log("Failed to connect to Database");
+  })
+}
+module.exports=connectDatabase;
